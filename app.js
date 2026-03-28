b7f3b8a6534afd252031950b97e6758d00d793bc
Vanessa Rosa <117919150+VanessaRosagit@users.noreply.github.com>
2026-03-28 10:33:36 -0300

    Alteração na atividade Funcionarios

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 'use strict'
 
 import funcionarios from "./funcionarios.json" with { type: "json" }
+function criarCard (funcionario){
+    const card =document.createElement('div')
+card.className = 'card'
+const foto = document.createElement('img')
+foto.src = `./img/${funcionario.imagem}`
+foto.alt = `./foto de ${funcionario.nome}`
+
+const nome = document.createElement('h3')
+nome.textContent = funcionario.nome
+
+const cargo = document.createElement('Span')
+cargo.textContent = funcionario.cargo
+
+card.append (foto,nome, cargo)
+return card
+
+
+}
+
+const cards = funcionarios.map(criarCard)
+document.getElementById('container').append(...cards)
+console.log (cards)
+
+
 
